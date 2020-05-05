9a68f2fe3b40ecb66a267f9751ee14b2400e561d
SLR1999 <lalithareddy.seelam@gmail.com>
2020-05-05 14:13:51 +0530

    changed 3 to 5 in code

--- frames.py
+++ frames.py
@@ -8,7 +8,7 @@ fps = int(vidcap.get(cv2.CAP_PROP_FPS))
 while success:
     success,image = vidcap.read()
     print('read a new frame:',success)
-    if count%(3*fps) == 0 :
+    if count%(5*fps) == 0 :
          cv2.imwrite('frame%d.jpg'%count,image)
          print('successfully written 10th frame')
     count+=1
