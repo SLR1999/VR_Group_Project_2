f02e85b0e084c9a56301bcbda52e42487767bbc1
SLR1999 <lalithareddy.seelam@gmail.com>
2020-05-05 12:11:37 +0530

    Getting frames from a video code

---
+++ frames.py
@@ -0,0 +1,14 @@
+import cv2
+
+vidcap = cv2.VideoCapture('testing.mp4')
+count = 0
+success = True
+fps = int(vidcap.get(cv2.CAP_PROP_FPS))
+
+while success:
+    success,image = vidcap.read()
+    print('read a new frame:',success)
+    if count%(3*fps) == 0 :
+         cv2.imwrite('frame%d.jpg'%count,image)
+         print('successfully written 10th frame')
+    count+=1
