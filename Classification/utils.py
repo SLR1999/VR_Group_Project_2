91026d558c0796fa27c44db8463d2e722c5a60e3
swastishreya <mishra.swastishreya13@gmail.com>
2020-05-15 01:47:27 +0530

    integrated yolo with attire classifier

--- Classification/utils.py
+++ Classification/utils.py
@@ -1,10 +1,10 @@
 import os
 import shutil
 
-path = "masked/"
+path = "yolo_images/"
 folders = [ item for item in os.listdir(path) if os.path.isdir(os.path.join(path, item)) ]
-train_path = "masked/train"
-test_path = "masked/val"
+train_path = "yolo_images/train"
+test_path = "yolo_images/val"
 
 for folder in folders:
     images = []
